7a221ec132be4a99fcc2eb02ff3466a9ea6ce2d3
Ryan Howard <ryanghoward@protonmail.com>
2022-07-24 09:32:30 -0700

    All rows rowing

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Movie from "./Movie";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -13,12 +13,12 @@ const Row = ({ title, fetchURL }) => {
   }, [fetchURL]);
 
   const slideLeft = () => {
-    var slider = document.getElementById("slider");
+    var slider = document.getElementById("slider" + rowID);
     slider.scrollLeft = slider.scrollLeft - 500;
   };
 
   const slideRight = () => {
-    var slider = document.getElementById("slider");
+    var slider = document.getElementById("slider" + rowID);
     slider.scrollLeft = slider.scrollLeft + 500;
   };
 
@@ -32,7 +32,7 @@ const Row = ({ title, fetchURL }) => {
           className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
         />
         <div
-          id={"slider"}
+          id={"slider" + rowID}
           className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
         >
           {movies.map((item, id) => (
