8ec05dd808fe9ca19c4be6ec6fcff0ac4c9cdb42
Ryan Howard <ryanghoward@protonmail.com>
2022-07-25 14:56:25 -0700

    User can successfully save movies to profile

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,7 +1,23 @@
 import React from "react";
+import SavedMovies from "../components/SavedMovies";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className='w-full text-white'>
+        <img
+          className='w-full h-[400px] object-cover'
+          src='https://assets.nflxext.com/ffe/siteui/vlv3/3a073c5f-0160-4d85-9a42-6f59aa4b64b9/3187860d-b0b5-4d82-88c3-034979c38e8b/US-en-20220718-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+          alt='/'
+        />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Saved Items</h1>
+        </div>
+      </div>
+      <SavedMovies />
+    </>
+  );
 };
 
 export default Account;
