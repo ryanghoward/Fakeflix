8b70b7035aa2e52e3c5661cbd3b026a9d3f725e6
Ryan Howard <ryanghoward@protonmail.com>
2022-07-25 10:29:24 -0700

    Changed backgrounds

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,12 +6,12 @@ const Login = () => {
     <div className='w-full h-screen'>
       <img
         className='hidden sm:block absolute w-full h-full object-cover'
-        src='https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/netflixteaser.png'
+        src='https://assets.nflxext.com/ffe/siteui/vlv3/3a073c5f-0160-4d85-9a42-6f59aa4b64b9/3187860d-b0b5-4d82-88c3-034979c38e8b/US-en-20220718-popsignuptwoweeks-perspective_alpha_website_large.jpg'
         alt='/'
       />
       <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
       <div className='fixed w-full px-4 py-24 z-50'>
-        <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
+        <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white rounded'>
           <div className='max-w-[320px] mx-auto py-16'>
             <h1 className='text-3xl font-bold'>Sign In</h1>
             <form className='w-full flex flex-col py-4'>
